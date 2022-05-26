e5f57e7bc86a6ac3ed70e42b67fbd0ad9e7d4f0f
Sreenath285 <sreenath.285@gmail.com>
2022-05-26 17:02:35 +0530

    Used lombok library for logging

--- src/main/java/com/sreenath/addressbook/AddressBookApplication.java
+++ src/main/java/com/sreenath/addressbook/AddressBookApplication.java
@@ -1,14 +1,18 @@
 package com.sreenath.addressbook;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
+@Slf4j
 public class AddressBookApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AddressBookApplication.class, args);
-        System.out.println("Welcome to Address Book Application");
+        ApplicationContext applicationContext = SpringApplication.run(AddressBookApplication.class, args);
+        log.info("Address Book App started in {} environment",
+                  applicationContext.getEnvironment().getProperty("environment"));
     }
 
 }
